c267641f0b46ce31c4367c67664c1d854225872a
ZhenZhouWu <Z486215937@gmail.com>
2022-05-04 17:20:11 +0800

    file

---
+++ fox/app/controllers/smallfox_controller.rb
@@ -0,0 +1,5 @@
+class SmallfoxController < ApplicationController
+	def eat
+		render plain: "吃冰"
+	end
+end
---
+++ fox/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  get '/smallfox/eat', to: 'smallfox#eat'
+end
