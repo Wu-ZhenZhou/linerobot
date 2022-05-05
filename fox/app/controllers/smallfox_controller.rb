acb1a7beeec90d81d3e9cbf14d85b4eeb18b716e
ZhenZhouWu <Z486215937@gmail.com>
2022-05-05 18:52:01 +0800

    2022/05/05-1

--- fox/app/controllers/smallfox_controller.rb
+++ fox/app/controllers/smallfox_controller.rb
@@ -2,4 +2,29 @@ class SmallfoxController < ApplicationController
 	def eat
 		render plain: "吃冰"
 	end
+
+	def request_headers
+		render plain: request.headers.to_h.reject{ |key, value|
+			key.include? '.'
+	}.map{ |key, value|
+			"#{key}: #{value}"
+		}.sort.join("\n")
+	end
+
+	def request_body
+		render plain: request.body
+	end
+
+	def response_headers
+		response.headers['7878'] = 'QQ'
+		render plain: response.headers.to_h.map{ |key, value|
+			"#{key}: #{value}"
+		}.sort.join("\n")
+	end
+
+	def show_response_body
+		puts "===這是設定前的response.body:#{response.body}==="
+    	render plain: "虎哇花哈哈哈"
+    	puts "===這是設定後的response.body:#{response.body}==="
+    end
 end
--- fox/config/routes.rb
+++ fox/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   get '/smallfox/eat', to: 'smallfox#eat'
+  get '/smallfox/request_headers', to: 'smallfox#request_headers'
+  get '/smallfox/request_body', to: 'smallfox#request_body'
+  get '/smallfox/response_headers', to: 'smallfox#response_headers'
+  get '/smallfox/response_body', to: 'smallfox#show_response_body'
+
 end
