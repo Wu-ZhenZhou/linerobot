b3d77b26d08a703ed1b7f0eb0a5af74d962f5693
Wu-ZhenZhou <Z486215937@gmail.com>
2022-05-07 15:52:20 +0800

    Heroku build

--- fox/config/routes.rb
+++ fox/config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/smallfox/request_body', to: 'smallfox#request_body'
   get '/smallfox/response_headers', to: 'smallfox#response_headers'
   get '/smallfox/response_body', to: 'smallfox#show_response_body'
-
+  get '/smallfox/sent_request', to: 'smallfox#sent_request'
+  
 end
