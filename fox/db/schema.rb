5d4abb1bc1acc01931460ebc119819ca1e123f43
Wu-ZhenZhou <Z486215937@gmail.com>
2022-05-21 15:39:01 +0800

    push up

--- fox/db/schema.rb
+++ fox/db/schema.rb
@@ -10,10 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_18_064820) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_21_063046) do
   create_table "keyword_mappings", force: :cascade do |t|
     t.string "keyword"
     t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "receiveds", force: :cascade do |t|
+    t.string "channel_id"
+    t.string "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "replies", force: :cascade do |t|
+    t.string "channel_id"
+    t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
