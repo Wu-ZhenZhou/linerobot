99379a442d2c6a9977b16fe1be31c830792d275e
Wu-ZhenZhou <Z486215937@gmail.com>
2022-05-25 15:32:59 +0800

    release trash

--- batfile/heroku_push.bat
+++
@@ -1,7 +0,0 @@
-cd ..
-cd fox
-git add .
-git commit -m "anyone to anyone"
-git push heroku master
-
-pause
--- batfile/toggle_smallfox.bat
+++
@@ -1,5 +0,0 @@
-D:
-cd smallfox
-cd heroku
-git config --global user.name smallfox
-git config --global user.email smallfox20010127@gmail.com
